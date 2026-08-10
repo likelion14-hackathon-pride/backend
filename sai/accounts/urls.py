bc8e08b084284159e6a4d0e3adc76963f45d7bdb
sangwon5579 <chosw0410@cau.ac.kr>
2026-08-10 21:04:17 +0900

    [fix] api 명세에 맞춰 수정

--- sai/accounts/urls.py
+++ sai/accounts/urls.py
@@ -8,7 +8,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path("signup", SignupView.as_view()),
+    path("signup/owner", OwnerSignupView.as_view()),
+    path("signup/member", MemberSignupView.as_view()),
     path("login", AuthView.as_view()),
     path("logout", LogoutView.as_view()),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
