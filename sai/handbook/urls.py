3103fcdb28038c711837f0ee554810336417b06d
yeankong <chayaeeun25@gmail.com>
2026-08-11 07:34:27 +0900

    [feat] handbook 항목별  상세 조회

--- sai/handbook/urls.py
+++ sai/handbook/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HandbookEntryListCreateView
+from .views import HandbookEntryDetailView, HandbookEntryListCreateView
 
 
 urlpatterns = [
     path('<int:company_id>/handbook/entries', HandbookEntryListCreateView.as_view()),
+    path('<int:company_id>/handbook/entries/<int:entry_id>', HandbookEntryDetailView.as_view()),
 ]
