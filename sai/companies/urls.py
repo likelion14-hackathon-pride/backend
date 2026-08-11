37de3c5653c4b4c3a960bbe83974ac8165a2cce0
yeankong <chayaeeun25@gmail.com>
2026-08-12 06:49:18 +0900

    [feat] 회사 구성원 목록 조회 API

--- sai/companies/urls.py
+++ sai/companies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CompanyDetailView
+from .views import CompanyDetailView, CompanyMemberListView
 
 
 urlpatterns = [
+    path('<int:company_id>/members', CompanyMemberListView.as_view()),
     path('<int:company_id>', CompanyDetailView.as_view()),
 ]
