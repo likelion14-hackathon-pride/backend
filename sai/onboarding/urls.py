88454f5beba46b4d5bb8230a203dd8a469b9a9ca
yeankong <chayaeeun25@gmail.com>
2026-08-12 08:05:04 +0900

    [feat] 온보딩 완료 화면 API

--- sai/onboarding/urls.py
+++ sai/onboarding/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import OnboardingQuestionDetailView, OnboardingView
+from .views import OnboardingCompleteView, OnboardingQuestionDetailView, OnboardingView
 
 
 urlpatterns = [
+    path('<int:company_id>/onboarding/complete', OnboardingCompleteView.as_view()),
     path('<int:company_id>/onboarding/questions/<int:question_id>', OnboardingQuestionDetailView.as_view()),
     path('<int:company_id>/onboarding', OnboardingView.as_view()),
 ]
