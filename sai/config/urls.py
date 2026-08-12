cfef46cd8c77dd03706e51b492d50fda4bc06583
sangwon5579 <chosw0410@cau.ac.kr>
2026-08-12 14:48:32 +0900

    [feat] Slack api

--- sai/config/urls.py
+++ sai/config/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
     path('api/companies/', include('policy.urls')),
     path('api/companies/', include('handbook.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("api/slack/", slack_events),
 ]
