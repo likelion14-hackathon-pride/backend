e3e669091200382471e8e12da8cdd6f6b843fb5d
yeankong <chayaeeun25@gmail.com>
2026-08-16 12:32:05 +0900

    [feat] S3 로컬 파일 다운로드 구현

--- sai/sources/local_files.py
+++ sai/sources/local_files.py
@@ -1,8 +1,16 @@
 import boto3
+from botocore.exceptions import BotoCoreError, ClientError
 from django.conf import settings
 
 
 UPLOAD_URL_EXPIRES = 15 * 60
+LOCAL_FILE_MAX_SIZE = 20 * 1024 * 1024
+
+
+class LocalFileStorageError(Exception):
+    def __init__(self, code):
+        self.code = code
+        super().__init__(code)
 
 
 # 파일은 Django 서버를 거치지 않고 S3에 바로 올린다.
@@ -26,3 +34,29 @@ def delete_file(storage_key):
         Bucket=settings.AWS_STORAGE_BUCKET_NAME,
         Key=storage_key,
     )
+
+
+def download_file(storage_key, expected_size):
+    client = boto3.client('s3', region_name=settings.AWS_S3_REGION_NAME)
+
+    try:
+        response = client.get_object(
+            Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+            Key=storage_key,
+        )
+        actual_size = response['ContentLength']
+        if actual_size != expected_size:
+            raise LocalFileStorageError('file_size_mismatch')
+        if actual_size > LOCAL_FILE_MAX_SIZE:
+            raise LocalFileStorageError('file_too_large')
+
+        return response['Body'].read(LOCAL_FILE_MAX_SIZE + 1)
+    except LocalFileStorageError:
+        raise
+    except ClientError as exc:
+        code = exc.response.get('Error', {}).get('Code')
+        if code in {'NoSuchKey', '404'}:
+            raise LocalFileStorageError('file_not_uploaded') from exc
+        raise LocalFileStorageError('storage_error') from exc
+    except BotoCoreError as exc:
+        raise LocalFileStorageError('storage_error') from exc
